79f169b36ea2718cc2f1fdb8d9d9fa15fa7beebd
MasterTemple <dgmastertemple@protonmail.com>
2021-09-12 03:04:11 -0700

    added role updating

---
+++ functions/discord/updateRoles.js
@@ -0,0 +1,31 @@
+module.exports = async (client, config) => {
+    return new Promise((resolve, reject) => {
+
+        let players = require('./../../data/playerData.json')
+        let members = await client.guilds.cache.get(guildId).members.fetch()
+        let rankRoles = {
+            "Kyber": "ID",
+            "Diamond": "ID",
+            "Gold": "ID",
+            "Silver": "ID",
+            "Bronze": "ID",
+        }
+
+        for(let player of players){
+
+            let member = members.get(player.ID)
+            let currentRoles = [...member.roles.cache.keys()]
+
+            for(let eachRankRole of Object.values(rankRoles)){
+
+                if(currentRoles.includes(eachRankRole)){
+                    await member.roles.remove(eachRankRole)
+                }
+
+            }
+            await member.roles.add(rankRoles[player.league])
+        }
+        resolve()
+    })
+
+}
